824cb90e41ef9309d61ceeacf7ec974dc7a04cce
Jean Nunes <jaon@itriad.org.br>
2020-03-29 05:41:00 -0400

    add appointment

--- src/main/java/com/medicalscheduling/api/controller/AppointmentController.java
+++ src/main/java/com/medicalscheduling/api/controller/AppointmentController.java
@@ -9,6 +9,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,6 +23,7 @@ import com.medicalscheduling.api.repository.AppointmentRepository;
 import com.medicalscheduling.api.repository.CustomerRepository;
 import com.medicalscheduling.api.repository.DoctorRepository;
 
+@CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 public class AppointmentController {
 	@Autowired
